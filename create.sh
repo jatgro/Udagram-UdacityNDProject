064850d3e90085b4dc915286fa6ace0d2ca53cb3
jg <er.jatin2112@gmail.com>
2020-06-28 11:44:57 +0530

    added commments in create.sh

--- create.sh
+++ create.sh
@@ -4,3 +4,7 @@ aws cloudformation create-stack
 --parameters file://$3 
 #--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" 
 --region=us-west-2
+
+# aws cloudformation create-stack --stack-name udagrm-network-infra-stack --template-body
+#  file://udagram-network-Infra.yml --parameters 
+# file://udagram-network-params.json --capabilitie "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
